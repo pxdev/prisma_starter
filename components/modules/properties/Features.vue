f3ab4804786fd8da88052d394d9f81e8962dac33
AHMED <ahmed@AHMEDs-iMac.local>
2024-07-30 17:04:35 +0300

    update

--- components/modules/properties/Features.vue
+++ components/modules/properties/Features.vue
@@ -9,45 +9,59 @@ const props = defineProps({
 </script>
 
 <template>
-  <div class="flex items-center text-sm justify-between">
+  <div class="flex items-center gap-4 text-sm justify-between">
 
     <u-tooltip :text="$t('property.beds')" v-if="props.beds">
-      <div class="flex gap-2 beds" >
+      <div class="flex gap-2 items-center" >
         <icon class="h-5 w-5" name="hugeicons:bed-single-01"/>
-        <p class="text-gray-500">
-          {{ props.beds }} {{ $t("property.beds") }}
-        </p>
+        <div class="text-gray-500">
+          <p class="block font-semibold">{{ props.beds }} </p>
+          <p class="block text-xs text-gray-400">{{$t('property.beds')}}</p>
+        </div>
+
+
       </div>
     </u-tooltip>
 
     <u-tooltip :text="$t('property.baths')" v-if="props.baths">
-    <div class="flex gap-2 baths" v-if="props.baths">
+    <div class="flex gap-2 items-center" v-if="props.baths">
       <icon class="h-5 w-5" name="hugeicons:bathtub-01"/>
-      <p class="text-gray-500">
-        {{ props.baths }} {{ $t("property.baths") }}
-      </p>
+      <div class="text-gray-500">
+        <p class="block font-semibold">{{ props.baths }}</p>
+        <p class="block text-xs text-gray-400">{{$t('property.baths')}}</p>
+      </div>
     </div>
     </u-tooltip>
 
     <u-tooltip :text="$t('property.area')" v-if="props.area">
-    <div class="flex gap-2 area" v-if="props.area">
+    <div class="flex gap-2 items-center" v-if="props.area">
       <icon class="h-5 w-5" name="iconoir:ruler-arrows"/>
-      <p class="text-gray-500">{{ props.area }} {{$t("property.sqft")}}</p>
-    </div>
+      <div class="text-gray-500">
+        <p class="block font-semibold">{{ props.area }} <span class="text-xs font-normal">{{$t("property.sqft")}}</span></p>
+        <p class="block text-xs text-gray-400">{{$t('property.area')}}</p>
+      </div>
+
+     </div>
     </u-tooltip>
 
     <u-tooltip :text="$t('property.garages')" v-if="props.garage">
     <div class="flex gap-2 area" v-if="props.garage">
       <icon class="h-5 w-5" name="ph:car-light"/>
-      <p class="text-gray-500">{{ props.garage }}</p>
+      <div class="text-gray-500">
+        <p class="block font-semibold">{{ props.garage }}</p>
+        <p class="block text-xs text-gray-400">{{$t('property.garages')}}</p>
+      </div>
     </div>
     </u-tooltip>
 
     <u-tooltip :text="$t('property.yearBuilt')" v-if="props.year">
     <div class="flex gap-2 area" v-if="props.year">
       <icon class="h-5 w-5" name="cil:calendar"/>
-      <p class="text-gray-500">{{ props.year }}</p>
-    </div>
+      <div class="text-gray-500">
+        <p class="block font-semibold">{{ props.year }}</p>
+        <p class="block text-xs text-gray-400">{{$t('property.yearBuilt')}}</p>
+      </div>
+     </div>
     </u-tooltip>
 
   </div>
--- components/modules/properties/PropertyGallery.vue
+++ components/modules/properties/PropertyGallery.vue
@@ -12,35 +12,57 @@ const setThumbsSwiper = (swiper) => {
   thumbsSwiper.value = swiper;
 }
 
+const sliderCurrent = ref(0);
+const sliderCount = ref(0);
+
+const onSwiperSlideChange = (swiper) => {
+  sliderCurrent.value = swiper.activeIndex + 1;
+  sliderCount.value = swiper.slides.length;
+};
+const onSwiperInit = (swiper) => {
+  sliderCount.value = swiper.slides.length;
+  sliderCurrent.value = swiper.activeIndex + 1;
+};
+
 </script>
 
 <template>
+  <u-card v-if="media.length > 0" class="mb-6">
+    <div class="mx-auto mb-4 bg-gray-900 relative w-full overflow-hidden">
 
-  <swiper
-      :modules="[SwiperFreeMode, SwiperNavigation, SwiperThumbs]"
-      :navigation="true"
-      :spaceBetween="10"
-      :thumbs="{ swiper: thumbsSwiper }"
-      class="mySwiper2"
-  >
-    <swiper-slide v-for="image in props.media ">
-      <img alt="" :src="`/uploads${image.path}`" />
-    </swiper-slide>
+      <swiper
+          :modules="[SwiperFreeMode, SwiperNavigation, SwiperThumbs]"
+          :thumbs="{ swiper: thumbsSwiper }"
 
-  </swiper>
-  <swiper
-      :freeMode="true"
-      :modules="[SwiperFreeMode, SwiperNavigation, SwiperThumbs]"
-      :slidesPerView="4"
-      :spaceBetween="10"
-      :watchSlidesProgress="true"
-      class="mySwiper"
-      @swiper="setThumbsSwiper"
-  >
-    <swiper-slide v-for="image in props.media ">
-      <img alt="" :src="`/uploads${image.path}`" />
-    </swiper-slide>
+          @init="onSwiperInit"
+          @slide-change="onSwiperSlideChange"
+      >
 
-  </swiper>
+        <swiper-slide v-for="image in props.media ">
+          <div class="w-full flex items-center justify-center overflow-hidden">
+            <img :src="`/uploads${image.path}`" alt="" class="block w-full h-full object-cover"/>
+          </div>
+        </swiper-slide>
+
+        <slider-nav v-if="media.length > 1" :count="sliderCount" :current="sliderCurrent"/>
+      </swiper>
+
+    </div>
+    <div v-if="media.length > 1" class="mx-auto relative">
+      <swiper
+          :freeMode="true"
+          :modules="[SwiperFreeMode, SwiperNavigation, SwiperThumbs]"
+          :slidesPerView="4"
+          :spaceBetween="10"
+          :watchSlidesProgress="true"
+          @swiper="setThumbsSwiper"
+      >
+        <swiper-slide v-for="image in props.media ">
+          <img :src="`/uploads${image.path}`" alt=""/>
+        </swiper-slide>
+
+      </swiper>
+    </div>
+  </u-card>
 </template>
 
